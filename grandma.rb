efcf67304aaf3920d22f96e9aeb2ee50e3ee2ad7
Terry Threatt <terry.threatt@gmail.com>
2020-02-13 15:44:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return puts 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase.upcase
-    return puts "NO, NOT SINCE 1938!"
-  else phrase
-   return puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  else 
+   puts "HUH?! SPEAK UP, SONNY!"
   end 
 end
 
